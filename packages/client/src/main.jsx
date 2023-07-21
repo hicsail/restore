55df9af9200fd2a4945455eb65c984351a10c21a
Zoe <19900057+vpsx@users.noreply.github.com>
2023-07-21 13:51:17 -0400

    Set up GraphQL on client with Apollo Client

--- packages/client/src/main.jsx
+++ packages/client/src/main.jsx
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const apolloclient = new ApolloClient({
+  uri: 'https://flyby-router-demo.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={apolloclient}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
 )
