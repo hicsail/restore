26e185bfc2b15e6212d893b8cadab8467fa16fe0
Zoe <19900057+vpsx@users.noreply.github.com>
2023-09-07 14:53:00 -0400

    Replace custom tab panels with routing

--- packages/client/src/components/App.jsx
+++ packages/client/src/components/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import '../App.css'
 
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 
 import { ThemeProvider } from '@mui/material/styles';
 import { Box, Tab, Tabs } from '@mui/material';
@@ -10,76 +10,22 @@ import PropTypes from 'prop-types';
 import { theme } from '../theme.jsx'
 
 
-function CustomTabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          {children}
-        </Box>
-      )}
-    </div>
-  );
-}
-
-CustomTabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
-
 function NavTabs() {
-  const [value, setValue] = useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+  const { pathname } = useLocation();
 
   return (
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="navigation tabs">
-          <Tab label="About" {...a11yProps(0)} />
-          <Tab label="Our Team" {...a11yProps(1)} />
-          <Tab label="Testimonials" {...a11yProps(2)} />
-          <Tab label="Research and Evaluation" {...a11yProps(3)} />
-          <Tab label="Get Involved" {...a11yProps(4)} />
-          <Tab label="Blog" {...a11yProps(5)} />
+        <Tabs value={pathname} aria-label="navigation tabs">
+          <Tab label='About' value='/about' to='about' component={Link} />
+          <Tab label='Our Team' value='/our-team' to='our-team' component={Link} />
+          <Tab label='Testimonials' value='/testimonials' to='testimonials' component={Link} />
+          <Tab label='Research and Evaluation' value='/research-and-evaluation' to='research-and-evaluation' component={Link} />
+          <Tab label='Get Involved' value='/get-involved' to='get-involved' component={Link} />
+          <Tab label='Blog' value='/blog' to='blog' component={Link} />
         </Tabs>
       </Box>
-      <CustomTabPanel value={value} index={0}>
-        <Outlet />
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={1}>
-        <Outlet />
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={2}>
-        <Outlet />
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={3}>
-        <Outlet />
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={4}>
-        <Outlet />
-      </CustomTabPanel>
-      <CustomTabPanel value={value} index={5}>
-        <Outlet />
-      </CustomTabPanel>
+      <Outlet />
     </Box>
   );
 }
