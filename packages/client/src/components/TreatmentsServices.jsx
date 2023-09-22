2c2f0e362a4b3c216b295f9b6c3d90e9396d938d
Zoe <19900057+vpsx@users.noreply.github.com>
2023-09-22 12:26:01 -0400

    Make interactive determinants diagram

--- packages/client/src/components/TreatmentsServices.jsx
+++ packages/client/src/components/TreatmentsServices.jsx
@@ -1,4 +1,5 @@
-import { Button } from '@mui/material';
+import { useState } from 'react';
+import { Box, Button, Popper } from '@mui/material';
 
 import boardsImg from '../assets/treatments_and_services/boards.gif'
 import implementationScienceFrameworksFullImg from '../assets/treatments_and_services/implementation_science_frameworks_full.png'
@@ -12,6 +13,134 @@ import proctorsTaxonomyImg from '../assets/treatments_and_services/proctors_taxo
 import { useQuery } from '@apollo/client';
 import { GET_UPCOMING_ONGOING } from '../gql.jsx'
 import ReactMarkdown from 'react-markdown';
+
+
+function DeterminantsInteractive () {
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [popperText, setPopperText] = useState(null);
+
+  const handleMouseOver = (ptext) => (event) => {
+    setAnchorEl(event.currentTarget);
+    setPopperText(ptext);
+  }
+  const handleMouseOut = (event) => {
+    setAnchorEl(null);
+    setPopperText(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'determinants-diagram-popper' : undefined;
+
+  const recipientsProvidersText = (
+    <>
+      <h3>Recipient: Provider</h3>
+      <h4><i>Problems</i></h4>
+      <ul>
+        <li>Provider practice shifts to using RESTORE supported evidence-based treatments and measurement-based care.</li>
+        <li>Provider skill-building in conceptualization of PTSD diagnosis and oppression-based stress.</li>
+        <li>Provider increase skill in fidelity modification to provide EBT in culturally responsive and patient centered approach.</li>
+      </ul>
+      <h4><i>RESTORE Response</i></h4>
+      <ul>
+        <li>All RESTORE clinicians are trained in 3 evidence-based interventions for PTSD and provided consultation on cultural adapted and responsive care.</li>
+        <li>All RESTEORE clinicians are trained in of measurement-based care.</li>
+      </ul>
+    </>
+  )
+  const recipientsPatientsText = (
+    <>
+      <h3>Recipient: Patient</h3>
+      <h4><i>Problems</i></h4>
+      <ul>
+        <li>Competing social (e.g., finance, housing) and psychological needs (e.g., trauma)</li>
+        <li>Barriers to care including resources (e.g., transportation, childcare) and personal (e.g., not wanting to discuss trauma)</li>
+      </ul>
+      <h4><i>RESTORE Response</i></h4>
+      <ul>
+        <li>Provide support to patients regarding barriers to treatment (e.g., virtual provider visits, technical assistance, and online self-paced treatment)</li>
+        <li>Support patient in building readiness for trauma treatment</li>
+      </ul>
+    </>
+  )
+  const theInnovationText = (
+    <>
+      <h3>The Innovation</h3>
+      <h4><i>Problems</i></h4>
+      <ul>
+        <li>Poor guidance from evidence-based interventions on how to integrate social context into PTSD treatment</li>
+        <li>Lack of language accessibility of evidence-based interventions</li>
+      </ul>
+      <h4><i>RESTORE Response</i></h4>
+      <ul>
+        <li>Our interventions have gone through a process of cultural adaptation</li>
+        <li>Our interventions have been translated in multiple language including Spanish and Haitian Creole</li>
+      </ul>
+    </>
+  )
+  const contextText = (
+    <>
+      <h3>Context</h3>
+      <h4><i>Problems</i></h4>
+      <ul>
+        <li>Scheduling difficulties with hospital system biweekly sessions</li>
+        <li>High provider turnover in the hospital system</li>
+        <li>System has limited time for provider training, consultation, and preparation for sessions</li>
+      </ul>
+      <h4><i>RESTORE Response</i></h4>
+      <ul>
+        <li>Our clinicians are provided with extra time for administrative work (e.g., preparing for sessions)</li>
+      </ul>
+    </>
+  )
+
+
+  return (
+    <>
+      <map name="determinants-venn-map">
+        <area
+          shape="circle"
+          coords="365,227,100"
+          alt="recipient: patient"
+          onMouseOver={handleMouseOver(recipientsPatientsText)}
+          onMouseOut={handleMouseOut}
+        />
+        <area
+          shape="circle"
+          coords="520,360,100"
+          alt="the innovation"
+          onMouseOver={handleMouseOver(theInnovationText)}
+          onMouseOut={handleMouseOut}
+        />
+        <area
+          shape="circle"
+          coords="365,500,100"
+          alt="recipient: provider"
+          onMouseOver={handleMouseOver(recipientsProvidersText)}
+          onMouseOut={handleMouseOut}
+        />
+        <area
+          shape="circle"
+          coords="360,360,300"
+          alt="context"
+          onMouseOver={handleMouseOver(contextText)}
+          onMouseOut={handleMouseOut}
+        />
+      </map>
+      <img
+        useMap="#determinants-venn-map"
+        src={ determinantsVennImg }
+        alt="determinants diagram"
+      />
+      <Popper id={id} open={open} anchorEl={anchorEl} placement='right'>
+        <Box sx={{ border: 1, p: 1, bgcolor: 'background.paper' }}>
+          {popperText}
+        </Box>
+      </Popper>
+    </>
+  )
+}
+
 
 
 export default function Services() {
@@ -83,7 +212,7 @@ export default function Services() {
       <h2 id="determinants"></h2>
       <p>Health Equity Implementation Framework proposes determinants believed to predict successful and equitable implementation.</p>
       <img src={ implementationFrameworkDeterminantsImg } alt="implementation framework: determinants" />
-      <img src={ determinantsVennImg } alt="determinants diagram" />
+      <DeterminantsInteractive />
 
       <h2 id="processes"></h2>
       <p>RESTORE provides facilitation—an interactive problem-solving approach that supports organizations in applying evidence-based practices in routine care.</p>
