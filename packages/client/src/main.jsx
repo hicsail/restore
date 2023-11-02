a5858c61fa61521eef9ff4bc66a9e63f15d22e7b
Zoe <19900057+vpsx@users.noreply.github.com>
2023-11-02 16:52:03 -0400

    Prettier

--- packages/client/src/main.jsx
+++ packages/client/src/main.jsx
@@ -1,86 +1,83 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 import { ApolloClient, createHttpLink, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { ThemeProvider } from '@mui/material/styles';
-import { theme } from './theme.jsx'
+import { theme } from './theme.jsx';
 import CssBaseline from '@mui/material/CssBaseline';
 
-import Layout from './components/Layout.jsx'
-import AboutMission from './pages/AboutMission.jsx'
-import TreatmentsServices from './pages/TreatmentsServices.jsx'
-import TeamMemberGrid from './pages/Team.jsx'
-import ResearchAndEvals from './pages/Research.jsx'
-import BlogPosts from './pages/Blog.jsx'
-import UnderConstruction from './pages/UnderConstruction.jsx'
+import Layout from './components/Layout.jsx';
+import AboutMission from './pages/AboutMission.jsx';
+import TreatmentsServices from './pages/TreatmentsServices.jsx';
+import TeamMemberGrid from './pages/Team.jsx';
+import ResearchAndEvals from './pages/Research.jsx';
+import BlogPosts from './pages/Blog.jsx';
+import UnderConstruction from './pages/UnderConstruction.jsx';
 
 const httpLink = createHttpLink({
-  uri: import.meta.env.VITE_STRAPI_URL + '/graphql',
+  uri: import.meta.env.VITE_STRAPI_URL + '/graphql'
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = import.meta.env.VITE_CONTENT_TOKEN
+  const token = import.meta.env.VITE_CONTENT_TOKEN;
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : "",
+      authorization: token ? `Bearer ${token}` : ''
     }
-  }
+  };
 });
 
 const apolloclient = new ApolloClient({
   link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache()
 });
-
-
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Layout />,
     children: [
       {
         index: true,
-        element: <AboutMission />,
+        element: <AboutMission />
       },
       {
-        path: "about",
-        element: <AboutMission />,
+        path: 'about',
+        element: <AboutMission />
       },
       {
-        path: "treatments-and-services",
-        element: <TreatmentsServices />,
+        path: 'treatments-and-services',
+        element: <TreatmentsServices />
       },
       {
-        path: "our-team",
-        element: <TeamMemberGrid />,
+        path: 'our-team',
+        element: <TeamMemberGrid />
       },
       {
-        path: "testimonials",
-        element: <UnderConstruction />,
+        path: 'testimonials',
+        element: <UnderConstruction />
       },
       {
-        path: "research-and-evaluation",
-        element: <ResearchAndEvals />,
+        path: 'research-and-evaluation',
+        element: <ResearchAndEvals />
       },
       {
-        path: "get-involved",
-        element: <UnderConstruction />,
+        path: 'get-involved',
+        element: <UnderConstruction />
       },
       {
-        path: "blog",
-        element: <BlogPosts />,
-      },
-    ],
-  },
+        path: 'blog',
+        element: <BlogPosts />
+      }
+    ]
+  }
 ]);
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -90,5 +87,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <RouterProvider router={router} />
       </ThemeProvider>
     </ApolloProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- packages/client/src/pages/UnderConstruction.jsx
+++ packages/client/src/pages/UnderConstruction.jsx
@@ -1,10 +1,9 @@
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from '../assets/react.svg';
+import viteLogo from '/vite.svg';
 
 export default function UnderConstruction() {
-
   return (
-    <div style={{textAlign: 'center'}}>
+    <div style={{ textAlign: 'center' }}>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} alt="Vite logo" />
@@ -19,9 +18,7 @@ export default function UnderConstruction() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Click on the Vite and React logos to learn more</p>
     </div>
-  )
+  );
 }
