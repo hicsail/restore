599bc915199ed70cc76da6e6553a59697bdadf5b
wenhwang97 <wenhwang@bu.edu>
2023-11-13 17:22:14 -0500

    add blog card component

---
+++ packages/client/src/components/BlogCard.jsx
@@ -0,0 +1,42 @@
+import { Link } from 'react-router-dom';
+import { Box, Card, CardActionArea, CardActions, CardContent, CardMedia, Typography } from '@mui/material';
+import { prependStrapiURL } from '../utils';
+
+export function BlogCard({ id, tag, title, description, imgSource, author, date }) {
+  return (
+    <Card
+      variant="outlined"
+      sx={{ display: 'flex', flexDirection: 'column', height: '100%', maxWidth: 380, border: 0, borderRadius: 0 }}
+    >
+      <CardActionArea
+        component={Link}
+        to={`${id}/${title.toLowerCase().replace(/[^a-z0-9]+/g, '-')}`}
+        sx={{ padding: 2, marginBottom: 'auto' }}
+      >
+        <CardMedia
+          component="img"
+          height={220}
+          image={imgSource ? prependStrapiURL(imgSource) : '/src/assets/imgplaceholder.png'}
+          sx={{ borderRadius: 4 }}
+        />
+        <CardContent sx={{ px: 0 }}>
+          <Typography variant="subtitle2" sx={{ fontWeight: 'bold' }} gutterBottom>
+            {tag}
+          </Typography>
+          <Typography variant="h6" sx={{ fontWeight: 'bold', lineHeight: 1.2 }} gutterBottom>
+            {title}
+          </Typography>
+          <Typography variant="body1">{description}</Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions sx={{ padding: 2 }}>
+        <Box>
+          <Typography variant="body2" sx={{ fontWeight: 'bold' }}>
+            {author}
+          </Typography>
+          <Typography variant="body2">{new Date(date).toDateString()}</Typography>
+        </Box>
+      </CardActions>
+    </Card>
+  );
+}
