e13718064abe87545bb5c1eec0e8f7693dc49f0c
Zoe <19900057+vpsx@users.noreply.github.com>
2023-12-01 11:55:19 -0500

    Speech bubble: smoosh the bubble

--- packages/client/src/components/SpeechBubble.jsx
+++ packages/client/src/components/SpeechBubble.jsx
@@ -5,10 +5,10 @@ export function SpeechBubble() {
     <svg width="1000" height="1000" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M 50 50
-           c 20 -20, 280 -20, 300 0
-           s 20 80, 0 100
+           c 20 -20, 270 -30, 300 0
+           s 20 80, -10 100
            s -280 20, -300 0
-           s -20 -80, 00 -100
+           s -20 -80, 10 -100
            z
           "
         fill={theme.palette.yellow.main}
