a5ee25d232530a683043fa9db037280a6f74097f
Zoe <19900057+vpsx@users.noreply.github.com>
2023-12-01 12:08:39 -0500

    Speech bubble: add pointy thingy

--- packages/client/src/components/SpeechBubble.jsx
+++ packages/client/src/components/SpeechBubble.jsx
@@ -7,8 +7,9 @@ export function SpeechBubble() {
         d="M 50 50
            c 20 -20, 270 -30, 300 0
            s 20 80, -10 100
-           s -280 20, -300 0
-           s -20 -80, 10 -100
+           s -250 -10, -290 20
+           s 1 -2, 0 -20
+           s -20 -80, -0 -100
            z
           "
         fill={theme.palette.yellow.main}
