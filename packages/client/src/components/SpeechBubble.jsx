e23053e234e6d2f4d8584bededf988520e7580bc
Zoe <19900057+vpsx@users.noreply.github.com>
2023-12-01 12:21:25 -0500

    Speech bubble: Round out bubble w pointy thing

--- packages/client/src/components/SpeechBubble.jsx
+++ packages/client/src/components/SpeechBubble.jsx
@@ -5,11 +5,11 @@ export function SpeechBubble() {
     <svg width="1000" height="1000" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M 50 50
-           c 20 -20, 270 -30, 300 0
+           c 30 -30, 270 -30, 300 0
            s 20 80, -10 100
-           s -250 -10, -290 20
-           s 1 -2, 0 -20
-           s -20 -80, -0 -100
+           s -250 -8, -300 20
+           s 6 -2, 0 -20
+           s -20 -80, 10 -100
            z
           "
         fill={theme.palette.yellow.main}
