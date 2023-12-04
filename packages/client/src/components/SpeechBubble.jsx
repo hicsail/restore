d7a5cc0398249ca1402acffef62f92257cacc131
Zoe <19900057+vpsx@users.noreply.github.com>
2023-12-04 11:35:46 -0500

    Preserve aspect ratio not; attempt speech bubble component

--- packages/client/src/components/SpeechBubble.jsx
+++ packages/client/src/components/SpeechBubble.jsx
@@ -1,15 +1,36 @@
 import { theme } from '../theme.jsx';
+import { Box, Typography } from '@mui/material';
 
 export function SpeechBubbleRight() {
   return (
-    <svg width="370" height="170" xmlns="http://www.w3.org/2000/svg">
+    <Box sx={{ position: 'relative' }}>
+      <Box sx={{ position: 'absolute', width: '100%', height: '100%', zIndex: -1, display: 'flex' }}>
+        <SpeechBubbleRightSVG />
+      </Box>
+      <Typography variant="h4" sx={{ padding: '4em 3em 6em 4em' }}>
+        This is text with a speech bubble behind it. The speech bubble should expand or contract according to the length
+        of the text. It... almost works... but try h4 and up/use a shorter text and watch what happens. Lorem ipsum
+        dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+        aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur...
+      </Typography>
+    </Box>
+  );
+}
+export function SpeechBubbleLeft() {
+  return 'To do';
+}
+
+export function SpeechBubbleRightSVG() {
+  return (
+    <svg viewBox="0 0 370 170" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
       <SpeechBubblePath />
     </svg>
   );
 }
-export function SpeechBubbleLeft() {
+export function SpeechBubbleLeftSVG() {
   return (
-    <svg width="370" height="170" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="0 0 370 170" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
       <g transform="scale(-1, 1) translate(-370,0)">
         <SpeechBubblePath />
       </g>
