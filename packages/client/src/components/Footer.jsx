5e7143066e8062b124c27b205e348c27ba6bc3ae
wenhwang97 <wenhwang@bu.edu>
2023-12-13 16:52:45 -0500

    integrate carousel to footer

--- packages/client/src/components/Footer.jsx
+++ packages/client/src/components/Footer.jsx
@@ -7,9 +7,15 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import YouTubeIcon from '@mui/icons-material/YouTube';
+import { Carousel } from './Carousel';
+import { useEffect, useRef, useState } from 'react';
 
 export const Footer = () => {
   const { loading, error, data } = useQuery(GET_LOGO_FULL_SVG);
+  const componentRef = useRef(null);
+  const [componentWidth, setComponentWidth] = useState(0);
+  const [displayedLogos, setDisplayedLogos] = useState(Math.floor((componentWidth - 100) / (200 + 20)));
+
   const socialLinks = [
     { name: 'Facebook', icon: <FacebookIcon />, url: 'https://www.facebook.com/' },
     { name: 'Twitter', icon: <TwitterIcon />, url: 'https://twitter.com/' },
@@ -17,13 +23,55 @@ export const Footer = () => {
     { name: 'LinkedIn', icon: <LinkedInIcon />, url: 'https://www.linkedin.com/' },
     { name: 'YouTube', icon: <YouTubeIcon />, url: 'https://www.youtube.com/' }
   ];
+  const logos = [
+    {
+      src: 'src/assets/bu-logo.webp',
+      url: 'https://www.bu.edu/'
+    },
+    {
+      src: 'src/assets/bmc-logo.png',
+      url: 'https://www.bmc.org/'
+    },
+    {
+      src: 'src/assets/nimh-logo.png',
+      url: 'https://www.nimh.nih.gov/'
+    },
+    {
+      src: 'src/assets/samhsa-logo.png',
+      url: 'https://www.samhsa.gov/'
+    },
+    {
+      src: 'src/assets/roots-and-wings-foundation-logo.png',
+      url: 'https://www.rootswings.org/'
+    },
+    {
+      src: 'src/assets/bcbs-ma-logo.jpeg',
+      url: 'https://www.bluecrossma.org/'
+    }
+  ];
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (componentRef.current) {
+        setComponentWidth(componentRef.current.offsetWidth);
+      }
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [data]);
+
+  useEffect(() => {
+    setDisplayedLogos(Math.floor((componentWidth - 100) / (200 + 20)));
+  }, [componentWidth]);
 
   if (loading) return <p>RESTORE</p>;
   if (error) return <p>Error : {error.message}</p>;
 
   return (
-    <>
-      <Container sx={{ marginX: 'auto', marginTop: 8, display: 'flex' }}>
+    <Box>
+      <Carousel logos={logos} displayCount={displayedLogos} />
+      <Container sx={{ marginX: 'auto', display: 'flex' }} ref={componentRef}>
         <Box>
           <Box
             component="img"
@@ -53,7 +101,6 @@ export const Footer = () => {
             ))}
           </Grid>
         </Box>
-        <Box component="img" src="src/assets/bu-logo.webp" height={80} marginLeft={5} marginTop="auto" />
       </Container>
       <Box justifyContent="center" marginBottom={8} paddingX={8}>
         <Divider sx={{ marginY: 3 }} />
@@ -66,6 +113,6 @@ export const Footer = () => {
           </Typography>
         </Box>
       </Box>
-    </>
+    </Box>
   );
 };
