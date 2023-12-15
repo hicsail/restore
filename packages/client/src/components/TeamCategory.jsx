028cba47468203f1dd8c0b373165c0b0a394200b
Zoe <19900057+vpsx@users.noreply.github.com>
2023-12-15 12:30:11 -0500

    Use toSorted instead of hacking shallow copy w filter

--- packages/client/src/components/TeamCategory.jsx
+++ packages/client/src/components/TeamCategory.jsx
@@ -14,8 +14,7 @@ export const TeamCategory = ({ TeamCategoryName, Description, team_members }) =>
       )}
       <Grid container spacing={2}>
         {team_members?.data
-          ?.filter(() => true)
-          .sort((a, b) => a.attributes.Order - b.attributes.Order)
+          ?.toSorted((a, b) => a.attributes.Order - b.attributes.Order)
           .map((member) => (
             <Grid item xs={12} sm={6} md={4} key={member.id}>
               <TeamMember {...member.attributes} />
