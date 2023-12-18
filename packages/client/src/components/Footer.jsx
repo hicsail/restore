10bf93c409098fab881f2d8a16f976baee643cb8
Wenhan Wang <wenhwang@bu.edu>
2023-12-18 11:26:30 -0500

    feat: add logo carousel to footer (#49)
    * remove links
    
    * create carousel component
    
    * add logo images
    
    * integrate carousel to footer
    
    * display at least one logo
    
    * adjust sizing and spacing for mobile
    
    * reconfigure carousel margin offset and display

--- packages/client/src/components/Footer.jsx
+++ packages/client/src/components/Footer.jsx
@@ -7,9 +7,15 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import YouTubeIcon from '@mui/icons-material/YouTube';
+import { Carousel } from './Carousel';
+import { useEffect, useRef, useState } from 'react';
 
 export const Footer = () => {
   const { loading, error, data } = useQuery(GET_LOGO_FULL_SVG);
+  const componentRef = useRef(null);
+  const [componentWidth, setComponentWidth] = useState(0);
+  const [displayedLogos, setDisplayedLogos] = useState(Math.floor((componentWidth - 120) / (200 + 40)));
+
   const socialLinks = [
     { name: 'Facebook', icon: <FacebookIcon />, url: 'https://www.facebook.com/' },
     { name: 'Twitter', icon: <TwitterIcon />, url: 'https://twitter.com/' },
@@ -17,13 +23,55 @@ export const Footer = () => {
     { name: 'LinkedIn', icon: <LinkedInIcon />, url: 'https://www.linkedin.com/' },
     { name: 'YouTube', icon: <YouTubeIcon />, url: 'https://www.youtube.com/' }
   ];
+  const logos = [
+    {
+      src: 'src/assets/bu-logo.webp',
+      url: 'https://www.bu.edu/'
+    },
+    {
+      src: 'src/assets/bmc-logo.png',
+      url: 'https://www.bmc.org/'
+    },
+    {
+      src: 'src/assets/nimh-logo.png',
+      url: 'https://www.nimh.nih.gov/'
+    },
+    {
+      src: 'src/assets/samhsa-logo.png',
+      url: 'https://www.samhsa.gov/'
+    },
+    {
+      src: 'src/assets/roots-and-wings-foundation-logo.png',
+      url: 'https://www.rootswings.org/'
+    },
+    {
+      src: 'src/assets/bcbs-ma-logo.jpeg',
+      url: 'https://www.bluecrossma.org/'
+    }
+  ];
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (componentRef.current) {
+        setComponentWidth(componentRef.current.offsetWidth);
+      }
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [data]);
+
+  useEffect(() => {
+    setDisplayedLogos(Math.floor((componentWidth - 120) / (200 + 40)));
+  }, [componentWidth]);
 
   if (loading) return <p>RESTORE</p>;
   if (error) return <p>Error : {error.message}</p>;
 
   return (
-    <>
-      <Container sx={{ marginX: 'auto', marginTop: 8, display: 'flex' }}>
+    <Box>
+      <Carousel logos={logos} displayCount={displayedLogos > 0 ? displayedLogos : 1} />
+      <Container display="flex" sx={{ marginX: 'auto', paddingX: { sm: 6, xs: 3 } }} ref={componentRef}>
         <Box>
           <Box
             component="img"
@@ -53,31 +101,35 @@ export const Footer = () => {
             ))}
           </Grid>
         </Box>
-        <Box component="img" src="/src/assets/bu_logo.webp" height={80} marginLeft={5} marginTop="auto" />
-        <Box display="flex" marginLeft="auto" gap={20}>
-          <Typography variant="body2" display="flex" flexDirection="column" gap={1}>
-            <Link>Link 1</Link>
-            <Link>Link 2</Link>
-            <Link>Link 3</Link>
-          </Typography>
-          <Typography variant="body2" display="flex" flexDirection="column" gap={1}>
-            <Link>Link 1</Link>
-            <Link>Link 2</Link>
-            <Link>Link 3</Link>
-          </Typography>
-        </Box>
       </Container>
-      <Box justifyContent="center" marginBottom={8} paddingX={8}>
-        <Divider sx={{ marginY: 3 }} />
-        <Box display="flex">
-          <Typography variant="body2">© 2023 All rights reserved</Typography>
-          <Typography variant="body2" sx={{ display: 'flex', gap: 5, marginLeft: 'auto' }}>
+      <Box display="flex" flexDirection="column" sx={{ marginBottom: 8 }}>
+        <Divider sx={{ alignSelf: 'center', marginY: 3, width: '90%' }} />
+        <Box
+          display="flex"
+          width="90%"
+          sx={{
+            flexDirection: { sm: 'row', xs: 'column-reverse' },
+            alignSelf: { sm: 'center', xs: 'start' },
+            paddingX: { sm: 0, xs: 3 }
+          }}
+        >
+          <Typography variant="body1">© 2023 All rights reserved</Typography>
+          <Typography
+            variant="body1"
+            display="flex"
+            sx={{
+              flexDirection: { sm: 'row', xs: 'column' },
+              gap: { sm: 5, xs: 2 },
+              marginLeft: { sm: 'auto', xs: 0 },
+              marginBottom: { sm: 0, xs: 2 }
+            }}
+          >
             <Link to="#">Privacy Policy</Link>
             <Link to="#">Terms of Service</Link>
             <Link to="#">Cookies Settings</Link>
           </Typography>
         </Box>
       </Box>
-    </>
+    </Box>
   );
 };
